40b227909157183e8a41b5fbcd1f6c82f4dd2f6e
chansung <pork1375@gmail.com>
2022-07-23 11:08:37 +0900

    stringTest

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("add ==> " + calculator.add(3, 4));
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,11 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -10,4 +13,34 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split1() {
+        String num = "1,2";
+        String[] numList = num.split(",");
+        System.out.println("---");
+        assertThat(numList[0]).contains("1");
+    }
+
+    @Test
+    void split2() {
+        String str = "(1,2)";
+        String aaa = str.substring(1, 4);
+        System.out.println(aaa);
+        assertThat(aaa).contains("1,2");
+    }
+
+    @Test
+    @DisplayName("abc위치값 넘지않기")
+    void split3() {
+        String abc = "abc";
+        try {
+            abc.charAt(5);
+        } catch (StringIndexOutOfBoundsException e) {
+            e.getMessage();
+        }
+    }
+
 }
+
+
